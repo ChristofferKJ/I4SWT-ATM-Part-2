8961c122ca5c253fd8557acd3b2ebab5c38563c2
Christofferkj <christoffer.k.jakobsen@gmail.com>
2018-11-13 17:44:25 +0100

    Added full coverage of log test

--- ATM/AirTrafficMonitor.Unit.Test/Tests/LogTest.cs
+++ ATM/AirTrafficMonitor.Unit.Test/Tests/LogTest.cs
@@ -24,7 +24,6 @@ namespace AirTrafficMonitor.Unit.Test.Tests
 
         [TestCase("1234", "1234", "kl.14")]
         [TestCase("abc", "ss", "kl.15")]
-        [TestCase("", "", "")]
         public void TestWriteToLog(string planeTagA, string planeTagB, string timeOfOccurrencce)
 
         {
@@ -36,6 +35,39 @@ namespace AirTrafficMonitor.Unit.Test.Tests
             }
 
         }
+
+        [TestCase("1234", "kl.14")]
+        [TestCase("ss", "kl.15")]
+        public void TestWriteLeavingPlaneToLog(string planeTagA, string TimeOfOccurrencce)
+        {
+            uut.WriteLeavingPlaneToLog(planeTagA, TimeOfOccurrencce);
+            using (sr = new StreamReader("Log.txt"))
+            {
+                string testLogLine = sr.ReadToEnd();
+                Assert.IsNotEmpty(testLogLine);
+            }
+
+        }
+
+        [TestCase("1234", "kl.14")]
+        [TestCase("ss", "kl.15")]
+        public void TestWriteEnteredPlaneToLog(string planeTagA, string TimeOfOccurrencce)
+        {
+            uut.WriteEnteredPlaneToLog(planeTagA, TimeOfOccurrencce);
+            using (sr = new StreamReader("Log.txt"))
+            {
+                string testLogLine = sr.ReadToEnd();
+                Assert.IsNotEmpty(testLogLine);
+            }
+        }
+
+        [Test]
+        public void TestClearLog()
+        {
+            using (sr=new StreamReader("Log.txt"))
+            Assert.IsEmpty(sr.ReadToEnd());
+        }
+
 
     }
 }
