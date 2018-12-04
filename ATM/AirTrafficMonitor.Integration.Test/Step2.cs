025093748f404e68d18ab5b534d2d6f5bcf3eefd
Hindkaer <hindkaer_msn@hotmail.com>
2018-12-04 12:17:22 +0100

    Integrationstest der virker er tilføjet

--- ATM/AirTrafficMonitor.Integration.Test/Step2.cs
+++ ATM/AirTrafficMonitor.Integration.Test/Step2.cs
@@ -19,6 +19,7 @@ namespace AirTrafficMonitor.Integration.Test
         private IRenedition fakerenedition;
         private List<IPlane> fakeplanelist;
         private IPlane fakeplane1;
+        private IPlane fakeplane2;
 
         [SetUp]
         public void Setup()
@@ -29,6 +30,7 @@ namespace AirTrafficMonitor.Integration.Test
             fakerenedition = Substitute.For<IRenedition>();
             fakeplanelist = Substitute.For<List<IPlane>>();
             fakeplane1 = Substitute.For<IPlane>();
+            fakeplane2 = Substitute.For<IPlane>();
 
             sut = new CheckPlanes(calcvelocity, calccourse, fakerenedition);
 
@@ -38,16 +40,59 @@ namespace AirTrafficMonitor.Integration.Test
             fakeplane1.YCoordinate = 30000;
             fakeplane1.Altitude = 17100;
 
+            fakeplane2 = new Plane();
+            fakeplane2.Tag = "ABC123";
+            fakeplane2.XCoordinate = 40000;
+            fakeplane2.YCoordinate = 35000;
+            fakeplane2.Altitude = 17100;
+
         }
 
         [Test]
-        public void Planes_in_list_is_there()
+        public void Plane_in_list_is_there()
+        {
+            fakeplanelist.Add(fakeplane1);
+            sut.CheckPlanesInAirspace(fakeplanelist);
+            Assert.That(fakeplanelist[0].Tag, Is.EqualTo(fakeplane1.Tag));
+        }
+
+        [Test]
+        public void Check_CalcCourse_Works_Correctly()
         {
             fakeplanelist.Add(fakeplane1);
             sut.CheckPlanesInAirspace(fakeplanelist);
 
-            fakerenedition.Received().render(fakeplanelist);
-        
+            fakeplanelist.Add(fakeplane2);
+            sut.CheckPlanesInAirspace(fakeplanelist);
+
+            double planex = fakeplane1.XCoordinate - fakeplane2.XCoordinate;
+            double planey = fakeplane1.YCoordinate - fakeplane2.YCoordinate;
+
+            double expected = Math.Atan2(planey, planex) * (180 / Math.PI);
+            if (expected < 0)
+            {
+                expected += 360;
+            }
+
+            Assert.AreEqual(expected, fakeplanelist[1].Course);
+        }
+
+        [Test]
+        public void Check_CalcVelocity_Works_Correctly()
+        {
+            fakeplanelist.Add(fakeplane1);
+            sut.CheckPlanesInAirspace(fakeplanelist);
+
+            fakeplanelist.Add(fakeplane2);
+            sut.CheckPlanesInAirspace(fakeplanelist);
+
+            double XYDiff = Math.Sqrt((Math.Pow((fakeplane2.XCoordinate - fakeplane1.XCoordinate), 2) + Math.Pow((fakeplane2.YCoordinate - fakeplane1.YCoordinate), 2)));
+            double AltitudeDiff = Math.Abs(fakeplane1.Altitude - fakeplane2.Altitude);
+            double DistanceMoved = Math.Sqrt((Math.Pow(XYDiff, 2) + Math.Pow(AltitudeDiff, 2)));
+            double TimeUsed = fakeplane2.TimeStamp.Subtract(fakeplane1.TimeStamp).TotalSeconds;
+            double expected = (DistanceMoved / TimeUsed);
+
+            Assert.AreEqual(expected, fakeplanelist[1].Velocity);
         }
     }
 }
